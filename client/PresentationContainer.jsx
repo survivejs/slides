f675e0b9f4a9c126768d23de6da555cfe1d0a952
Juho Vepsalainen <bebraw@gmail.com>
2018-10-14 11:45:28 +0300

    refactor: Separate slides component

--- client/PresentationContainer.jsx
+++ client/PresentationContainer.jsx
@@ -1,19 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Swipe from "react-swipe-component";
-import Observer from "react-intersection-observer";
-import connect from "./utils/connect";
-import ScheduleIcon from "./components/ScheduleIcon.jsx";
-import TitlePage from "./components/TitlePage.jsx";
-import logo from "./assets/colored-logo.svg";
 import root from "window-or-global";
+import Slides from "./components/Slides.jsx";
+import connect from "./utils/connect";
 
 // TODO: Lazy load as in https://www.npmjs.com/package/react-intersection-observer#polyfill
 if (root.location) {
   require("intersection-observer");
 }
-
-const styles = {}; // TODO
 
 class PresentationContainer extends React.Component {
   state = {
@@ -90,71 +85,6 @@ PresentationContainer.propTypes = {
   theme: PropTypes.object
 };
 
-function Slides({ schedule, theme, onSlideVisible }) {
-  function onSlideChange(slide) {
-    return inView => {
-      if (inView) {
-        onSlideVisible(slide);
-      }
-    };
-  }
-
-  return (
-    <>
-      <div className={`${styles.titlePageContainer} slide-0`}>
-        <Observer onChange={onSlideChange(0)}>
-          <TitlePage theme={theme} />
-        </Observer>
-      </div>
-      {schedule ? (
-        schedule.intervals
-          .map(interval => interval.sessions[0])
-          .map((session, index) => (
-            <Observer key={index} onChange={onSlideChange(index)}>
-              <div
-                className={`${styles.presentationContainer} slide-${index + 1}`}
-              >
-                <header className={styles.presentationHeader}>
-                  <div className={styles.presentationLogoContainer}>
-                    <img
-                      src={logo}
-                      alt="GraphQL Finland 2018"
-                      className={styles.presentationLogo}
-                    />
-                  </div>
-                  <div />
-                  {session.speakers && (
-                    <div className={styles.speakerImageContainer}>
-                      <img
-                        className={styles.speakerImage}
-                        src={session.speakers[0].image.url}
-                        alt={session.speakers[0].name}
-                      />
-                    </div>
-                  )}
-                </header>
-                <main className={styles.presentationContent}>
-                  <SessionTitle
-                    className={styles.presentationTitle}
-                    {...session}
-                  />
-                  {session.interval && (
-                    <h4 className={styles.presentationInterval}>
-                      {session.interval.begin} - {session.interval.end}
-                    </h4>
-                  )}
-                </main>
-                <footer className={styles.presentationFooter} />
-              </div>
-            </Observer>
-          ))
-      ) : (
-        <React.Fragment />
-      )}
-    </>
-  );
-}
-
 function getSlide() {
   if (!root.location) {
     return 0;
@@ -162,30 +92,6 @@ function getSlide() {
 
   return root.location.hash ? parseInt(root.location.hash.slice(1)) : 0;
 }
-
-const SessionTitle = ({ className, title, type, speakers }) => (
-  <h3 className={className}>
-    {type === "COFFEE_BREAK" ||
-    type === "PARTY" ||
-    type === "LUNCH" ||
-    type === "BREAKFAST" ? (
-      <div className={styles.specialSession}>
-        {title} <ScheduleIcon type={type} />
-      </div>
-    ) : (
-      <>
-        <div className={styles.speakerTitle}>{title}</div>
-        {speakers ? (
-          <span className={styles.speakerName}>
-            {speakers[0].name} <ScheduleIcon type={type} />
-          </span>
-        ) : (
-          <ScheduleIcon type={type} />
-        )}
-      </>
-    )}
-  </h3>
-);
 
 export default connect(
   `
