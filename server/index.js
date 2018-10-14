fa1946d7b7a00210e3ad636a7e4bff745cb98d33
Juho Vepsalainen <bebraw@gmail.com>
2018-10-14 10:28:53 +0300

    chore: Sketch out initial slide schema

--- server/index.js
+++ server/index.js
@@ -1,25 +1,32 @@
 /* eslint-disable no-console */
+const path = require("path");
 const { GraphQLServer } = require("graphql-yoga");
+const { importSchema } = require("graphql-import");
 
-const typeDefs = `
-type Query {
-  theme: Theme
-}
+const theme = {
+  primaryColor: "#09b5c4",
+  secondaryColor: "#19a0ab94"
+};
 
-type Theme {
-  primaryColor: String!
-  secondaryColor: String!
-}
-`;
+const slides = [
+  {
+    layout: "TITLE",
+    content: {
+      title: "Brief Introduction to GraphQL",
+      author: "Juho Vepsäläinen"
+    }
+  }
+];
 
 const resolvers = {
   Query: {
-    theme: () => ({
-      primaryColor: "#09b5c4",
-      secondaryColor: "#19a0ab94"
-    })
+    theme: () => theme,
+    slides: () => slides
   }
 };
 
-const server = new GraphQLServer({ typeDefs, resolvers });
+const server = new GraphQLServer({
+  typeDefs: importSchema(path.resolve(__dirname, "schema.graphql")),
+  resolvers
+});
 server.start(() => console.log("Server is running on localhost:4000"));
