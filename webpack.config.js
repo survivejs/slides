deb2ff23e553009c1c6ca965ff40171f03721ddd
Juho Vepsalainen <bebraw@gmail.com>
2018-10-14 17:45:42 +0300

    chore: Simplify configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,74 +1,55 @@
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const merge = require("webpack-merge");
 
-module.exports = env => {
-  switch (env) {
-    case "build":
-    case "interactive":
-      return commonConfig(env);
-    default:
-      return merge(commonConfig(env), developmentConfig());
-  }
-};
-
-function commonConfig(env) {
-  return {
-    stats: "minimal",
-    module: {
-      rules: [
-        {
-          test: /\.js(x)$/,
-          use: [
-            "babel-loader",
-            {
-              loader: "@bebraw/linaria/loader",
-              options: {
-                sourceMap: env === "develop"
-              }
+module.exports = env => ({
+  stats: "minimal",
+  module: {
+    rules: [
+      {
+        test: /\.js(x)$/,
+        use: [
+          "babel-loader",
+          {
+            loader: "@bebraw/linaria/loader",
+            options: {
+              sourceMap: env === "develop"
             }
-          ],
-          include: path.join(__dirname, "client")
-        },
-        {
-          test: /\.css$/,
-          use: [
-            MiniCssExtractPlugin.loader,
-            {
-              loader: "css-loader",
-              options: {
-                sourceMap: env === "develop"
-              }
+          }
+        ],
+        include: path.join(__dirname, "client")
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: env === "develop"
             }
-          ]
-        },
-        {
-          test: /\.woff(2)?|\.ttf$|\.eot$/,
-          use: [
-            {
-              loader: "url-loader",
-              options: {
-                limit: 5000
-              }
+          }
+        ]
+      },
+      {
+        test: /\.woff(2)?|\.ttf$|\.eot$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 5000
             }
-          ]
-        },
-        {
-          test: /\.(gif|jpg|png|svg)$/,
-          use: "file-loader"
-        },
-        {
-          test: /\.(md|txt)$/,
-          use: "raw-loader"
-        }
-      ]
-    },
-    plugins: [new MiniCssExtractPlugin()]
-  };
-}
-
-function developmentConfig() {
-  return {
-    stats: "minimal"
-  };
-}
+          }
+        ]
+      },
+      {
+        test: /\.(gif|jpg|png|svg)$/,
+        use: "file-loader"
+      },
+      {
+        test: /\.(md|txt)$/,
+        use: "raw-loader"
+      }
+    ]
+  },
+  plugins: [new MiniCssExtractPlugin()]
+});
