5998e0bdc3c9c5ff8338d8a153541e7b2456c870
Juho Vepsalainen <bebraw@gmail.com>
2018-10-14 13:00:14 +0300

    chore: Set up initial page rendering customization for SSR
    I think this means the GraphQL queries have to live inside Interactive
    as otherwise this won't work.

--- antwar.config.js
+++ antwar.config.js
@@ -1,5 +1,8 @@
 module.exports = () => ({
   output: "build",
+  render: {
+    page: require("./utils/render-page")
+  },
   paths: {
     "/": () => require("./client/PresentationPage.jsx").default
   }
