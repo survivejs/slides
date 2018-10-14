e43854b40d1b55424470a467cbc963f24e5063d3
Juho Vepsalainen <bebraw@gmail.com>
2018-10-14 12:14:05 +0300

    chore: Integrate markdown rendering

--- client/components/layouts/MarkdownContent.jsx
+++ client/components/layouts/MarkdownContent.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 import { styled } from "@bebraw/linaria/react";
 import { modularScale } from "polished";
+import ReactMarkdown from "react-markdown";
 
 const MarkdownContainer = styled.div`
   min-height: 100vh;
   max-height: 100vh;
   display: grid;
-  grid-template-rows: repeat(2, 1fr);
+  grid-template-rows: 0.5fr 1.5fr;
   align-items: center;
 `;
 
@@ -19,15 +20,17 @@ const Title = styled.h1`
 
 const Markup = styled.h2`
   font-size: ${modularScale(4)};
-  margin-right: 5vw;
-  justify-self: end;
+  margin-left: 5vw;
+  align-self: start;
   color: ${props => props.color};
 `;
 
 const MarkdownContent = ({ content = {}, theme = {} }) => (
   <MarkdownContainer>
     <Title color={theme.primaryColor}>{content.title}</Title>
-    <Markup color={theme.secondaryColor}>{content.markup}</Markup>
+    <Markup color={theme.secondaryColor}>
+      <ReactMarkdown source={content.markup} />
+    </Markup>
   </MarkdownContainer>
 );
 MarkdownContent.propTypes = {
