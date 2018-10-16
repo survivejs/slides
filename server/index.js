02ac63191a6981cabe6b67fa8f64efb8f30c810c
Juho Vepsalainen <bebraw@gmail.com>
2018-10-16 19:15:19 +0300

    Revert "refactor(server): Rename schema file to JS"
    This reverts commit fac97e41c58b9983bca11fc013288a252b1cbd39.

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 /* eslint-disable no-console */
 const path = require("path");
 const { GraphQLServer } = require("graphql-yoga");
-const typeDefs = require("./schema");
+const { importSchema } = require("graphql-import");
 const resolvers = require("./resolvers");
 
-const server = new GraphQLServer({ typeDefs, resolvers });
+const server = new GraphQLServer({
+  typeDefs: importSchema(path.resolve(__dirname, "schema.graphql")),
+  resolvers
+});
 server.start(() => console.log("Server is running on localhost:4000"));
