ca07f00e8b460f25a51119ac43fc7eb27e3fcf74
Juho Vepsalainen <bebraw@gmail.com>
2018-10-28 15:47:17 +0100

    chore: Add a stub for a presentation

--- server/presentations/index.js
+++ server/presentations/index.js
@@ -3,5 +3,10 @@ const { loadYAML } = require("../utils");
 
 // TODO: Clean up
 module.exports = {
-  "intro-to-graphql": loadYAML(path.resolve(__dirname, "intro-to-graphql.yaml"))
+  "intro-to-graphql": loadYAML(
+    path.resolve(__dirname, "intro-to-graphql.yaml")
+  ),
+  "lets-make-a-graphql-presentation": loadYAML(
+    path.resolve(__dirname, "lets-make-a-graphql-presentation.yaml")
+  )
 };
